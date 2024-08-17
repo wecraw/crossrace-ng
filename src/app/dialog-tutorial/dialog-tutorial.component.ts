5554154ee34d7ecd13df532eae407f8cfd0833f9
wecraw <will@ccrawford.org>
2024-08-17 15:42:12 -0700

    tutorial styles complete

--- src/app/dialog-tutorial/dialog-tutorial.component.ts
+++ src/app/dialog-tutorial/dialog-tutorial.component.ts
@@ -39,19 +39,22 @@ const grids = [
     ['', '', '', '', '', '', ''],
     ['', '', '', 'B', 'E', '', ''],
     ['', '', '', 'R', '', '', ''],
-    ['', 'D', 'A', 'I', 'L', '', ''],
+    ['', 'D', 'A', 'I', 'L', 'Y', ''],
     ['', '', '', 'G', '', '', ''],
     ['', '', '', 'T', 'O', '', ''],
     ['', '', '', '', '', '', ''],
   ],
   [
-    ['', '', '', '', '', '', ''],
-    ['', '', '', 'B', '', '', ''],
-    ['', '', '', 'R', '', '', ''],
-    ['', 'D', 'A', 'I', 'L', 'Y', ''],
-    ['', '', '', 'G', '', '', ''],
-    ['', 'J', 'E', 'T', '', '', ''],
-    ['', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', 'B', '', '', '', ''],
+    ['', '', '', '', '', 'R', '', '', '', ''],
+    ['', '', '', 'D', 'A', 'I', 'L', '', '', ''],
+    ['', '', '', '', '', 'G', '', '', '', ''],
+    ['', '', '', 'J', 'E', 'T', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', '', ''],
   ],
 ];
 
@@ -78,13 +81,16 @@ const descriptions = [
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DialogTutorial implements OnInit, AfterViewInit {
-  @ViewChild('nextButton') nextButton!: ElementRef<HTMLButtonElement>;
-
   isAnimationFinished: boolean = false;
   isAnimationStarted: boolean = false;
   showTouch: boolean = false;
   scene: number = 0;
   descriptions: string[] = descriptions;
+  isAnimation2Started: boolean = false;
+  showTouch2: boolean = false;
+  isAnimation2Finished: boolean = false;
+  timeouts: any[] = [];
+
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
@@ -102,8 +108,11 @@ export class DialogTutorial implements OnInit, AfterViewInit {
   activeGrid: string[][] = this.grids[0];
 
   ngOnInit() {
-    //
-    let gridSize = 7;
+    this.generateGrid();
+  }
+
+  generateGrid() {
+    let gridSize = this.grids[this.scene].length;
     this.grid = Array(gridSize)
       .fill(0)
       .map(() => Array(gridSize).fill(0));
@@ -111,29 +120,64 @@ export class DialogTutorial implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.ngZone.run(() => {
-      setTimeout(() => {
+      const timeout1 = setTimeout(() => {
         this.startAnimation();
-      }, 500);
-    });
-    setTimeout(() => {
-      this.nextButton.nativeElement.focus();
+      }, 400);
+      this.timeouts.push(timeout1);
     });
   }
 
-  startAnimation() {
+  startAnimation(offset?: boolean) {
+    this.resetAnimation1();
     this.ngZone.run(() => {
       this.showTouch = true;
-      setTimeout(() => {
+      const timeout1 = setTimeout(() => {
         this.isAnimationStarted = true;
         this.cdr.detectChanges(); // Manually trigger change detection
       }, 500);
+      this.timeouts.push(timeout1);
 
-      setTimeout(() => {
+      const timeout2 = setTimeout(() => {
         this.isAnimationFinished = true;
         this.cdr.detectChanges(); // Manually trigger change detection
-      }, 1700);
+      }, 1600);
+      this.timeouts.push(timeout2);
     });
   }
+
+  resetAnimation1() {
+    this.isAnimationStarted = false;
+    this.showTouch = false;
+    this.isAnimationFinished = false;
+  }
+
+  resetAnimation2() {
+    this.isAnimation2Started = false;
+    this.showTouch2 = false;
+    this.isAnimation2Finished = false;
+  }
+
+  startAnimation2() {
+    this.resetAnimation1();
+    this.resetAnimation2();
+
+    this.ngZone.run(() => {
+      this.showTouch2 = true;
+      const timeout1 = setTimeout(() => {
+        this.isAnimation2Started = true;
+        this.cdr.detectChanges();
+      }, 300);
+      this.timeouts.push(timeout1);
+
+      const timeout2 = setTimeout(() => {
+        this.showTouch2 = false;
+        this.startAnimation();
+        this.cdr.detectChanges();
+      }, 1400);
+      this.timeouts.push(timeout2);
+    });
+  }
+
   close() {
     this.dialogRef.close();
   }
@@ -147,7 +191,7 @@ export class DialogTutorial implements OnInit, AfterViewInit {
   }
 
   checkBlueLetter(letter: string) {
-    if (this.scene === 0) {
+    if (this.scene === 0 || this.scene === 2) {
       return (
         letter.length > 0 &&
         'DAIL'.includes(letter) &&
@@ -160,12 +204,24 @@ export class DialogTutorial implements OnInit, AfterViewInit {
     }
   }
 
+  clearAllTimeouts() {
+    this.timeouts.forEach(clearTimeout);
+    this.timeouts.length = 0;
+  }
+
   updateScene() {
+    this.clearAllTimeouts();
+    this.resetAnimation1();
+    this.resetAnimation2;
+    if (this.scene === 2) {
+      this.startAnimation2();
+    } else {
+      this.isAnimation2Started = false;
+    }
+    this.generateGrid();
     this.activeGrid = this.grids[this.scene];
 
     if (this.scene === 0) {
-      this.isAnimationStarted = false;
-      this.isAnimationFinished = false;
       this.startAnimation();
     }
   }
