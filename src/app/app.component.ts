d7c78397abb6c13fdcaef07655d9df29592b1bc1
wecraw <will@ccrawford.org>
2024-08-22 11:43:52 -0700

    prevent double tap to zoom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Inject,
+  Renderer2,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { LobbyComponent } from './lobby/lobby.component';
 import { HeaderComponent } from './header/header.component';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +20,31 @@ import { HeaderComponent } from './header/header.component';
 })
 export class AppComponent {
   title = 'crossrace-ng';
+
+  private lastTap = 0;
+  private doubleTapThreshold = 300; // milliseconds
+
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  ngOnInit() {
+    // Prevent double tap zoom on iOS devices
+    this.renderer.listen('document', 'touchstart', (event: TouchEvent) => {
+      const currentTime = new Date().getTime();
+      const tapLength = currentTime - this.lastTap;
+
+      if (tapLength < this.doubleTapThreshold && tapLength > 0) {
+        event.preventDefault();
+        console.log('Double tap prevented');
+      }
+
+      this.lastTap = currentTime;
+    });
+
+    this.renderer.listen('document', 'gesturestart', (event: Event) => {
+      event.preventDefault();
+    });
+  }
 }
