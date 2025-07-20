67edb1f69c49483254fe9bd2bd244ca5638674a5
wecraw <will@ccrawford.org>
2025-07-20 00:32:01 -0700

    main menu positioning tweak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,10 +6,10 @@ export const environment: Environment = {
 
   version: {
     number: '0.9.0',
-    buildNumber: 1752996227,
-    buildDate: '2025-07-20T07:23:47.946Z',
-    gitHash: 'd7e9d62',
-    gitBranch: 'project-structure',
-    displayVersion: '0.9.0.1752996227'
+    buildNumber: 1752996378,
+    buildDate: '2025-07-20T07:26:18.629Z',
+    gitHash: 'f368519',
+    gitBranch: 'dev',
+    displayVersion: '0.9.0.1752996378'
   }
 };
