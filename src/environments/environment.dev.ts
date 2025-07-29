cd14caa7ae471b8d9c7779bf6c238b71544a4f55
wecraw <will@ccrawford.org>
2025-07-29 12:33:00 -0700

    prevent letter shift on pulsate

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -6,10 +6,10 @@ export const environment: Environment = {
 
   version: {
     number: '0.9.0',
-    buildNumber: 1753813208,
-    buildDate: '2025-07-29T18:20:08.632Z',
-    gitHash: '140f488',
+    buildNumber: 1753815342,
+    buildDate: '2025-07-29T18:55:42.941Z',
+    gitHash: '47751fe',
     gitBranch: 'post-game-refactor',
-    displayVersion: '0.9.0.1753813208 (dev)'
+    displayVersion: '0.9.0.1753815342 (dev)'
   }
 };
