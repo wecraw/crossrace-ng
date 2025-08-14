85a757facece8fa6182a3dc15d3a986902d3dbc4
wecraw <will@ccrawford.org>
2025-08-13 21:57:41 -0700

    alignment for host game form

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,10 +6,10 @@ export const environment: Environment = {
 
   version: {
     number: '0.10.2',
-    buildNumber: 1755146229,
-    buildDate: '2025-08-14T04:37:09.195Z',
-    gitHash: '14fab8e',
+    buildNumber: 1755146757,
+    buildDate: '2025-08-14T04:45:57.767Z',
+    gitHash: '1c7cc74',
     gitBranch: 'force-name',
-    displayVersion: '0.10.2.1755146229'
+    displayVersion: '0.10.2.1755146757'
   }
 };
