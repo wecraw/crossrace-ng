27f322c611663576c2e7be5e94089bc5e9e5099e
wecraw <will@ccrawford.org>
2025-08-21 23:12:38 -0700

    css cleanup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,10 +6,10 @@ export const environment: Environment = {
 
   version: {
     number: '0.11.0',
-    buildNumber: 1755840891,
-    buildDate: '2025-08-22T05:34:51.381Z',
-    gitHash: 'b026a32',
+    buildNumber: 1755842369,
+    buildDate: '2025-08-22T05:59:29.732Z',
+    gitHash: 'cdb986d',
     gitBranch: 'feature/dark-mode',
-    displayVersion: '0.11.0.1755840891'
+    displayVersion: '0.11.0.1755842369'
   }
 };
