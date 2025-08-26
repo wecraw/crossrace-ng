bde39b9230b6d7f9506eb33792f3a1ee395ff90f
Will Crawford <will@ccrawford.org>
2025-08-26 09:50:13 -0700

    hover button styles, leaderboard styles, cleanup

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -6,10 +6,10 @@ export const environment: Environment = {
 
   version: {
     number: '0.11.0',
-    buildNumber: 1756182938,
-    buildDate: '2025-08-26T04:35:38.032Z',
-    gitHash: '41610cf',
+    buildNumber: 1756226607,
+    buildDate: '2025-08-26T16:43:27.225Z',
+    gitHash: '80cf091',
     gitBranch: 'feature/dark-mode',
-    displayVersion: '0.11.0.1756182938 (dev)'
+    displayVersion: '0.11.0.1756226607 (dev)'
   }
 };
