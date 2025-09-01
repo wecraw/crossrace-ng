4a3bb723e8cf436bcba1e487d8ddbec76c764b45
Will Crawford <will@ccrawford.org>
2025-09-01 10:28:53 -0700

    cleanup wordlist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,10 +6,10 @@ export const environment: Environment = {
 
   version: {
     number: '0.11.0',
-    buildNumber: 1756351232,
-    buildDate: '2025-08-28T03:20:32.274Z',
-    gitHash: '1b1c155',
+    buildNumber: 1756747424,
+    buildDate: '2025-09-01T17:23:44.957Z',
+    gitHash: 'e464429',
     gitBranch: 'dev',
-    displayVersion: '0.11.0.1756351232'
+    displayVersion: '0.11.0.1756747424'
   }
 };
