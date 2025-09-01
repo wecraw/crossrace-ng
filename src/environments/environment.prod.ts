e78344642d4bc785224034069e2e4cbafc72e48e
Will Crawford <will@ccrawford.org>
2025-09-01 11:17:26 -0700

    version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,11 +5,11 @@ export const environment: Environment = {
   serverUrl: '__SERVER_URL__',
 
   version: {
-    number: '0.11.0',
-    buildNumber: 1756747424,
-    buildDate: '2025-09-01T17:23:44.957Z',
-    gitHash: 'e464429',
+    number: '0.12.0',
+    buildNumber: 1756750609,
+    buildDate: '2025-09-01T18:16:49.604Z',
+    gitHash: '134b5ac',
     gitBranch: 'dev',
-    displayVersion: '0.11.0.1756747424'
+    displayVersion: '0.12.0.1756750609'
   }
 };
