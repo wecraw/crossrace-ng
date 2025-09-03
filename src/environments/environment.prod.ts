ca0d3752f72dd654708796cc0c951c0510f0819b
Will Crawford <will@ccrawford.org>
2025-09-02 17:16:48 -0700

    increase font size for game board tiles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,10 +6,10 @@ export const environment: Environment = {
 
   version: {
     number: '0.12.0',
-    buildNumber: 1756766942,
-    buildDate: '2025-09-01T22:49:02.253Z',
-    gitHash: '12b5c3b',
-    gitBranch: 'grid-upsize',
-    displayVersion: '0.12.0.1756766942'
+    buildNumber: 1756857653,
+    buildDate: '2025-09-03T00:00:53.364Z',
+    gitHash: '1d45512',
+    gitBranch: 'dev',
+    displayVersion: '0.12.0.1756857653'
   }
 };
